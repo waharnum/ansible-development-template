1715767b2baae50d25415caa1e820c813b54c19c
Alan Harnum <aharnum@ocadu.ca>
2015-12-09 12:28:57 -0500

    development work on haproxy-docker role

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Clearing the vagrant box IPs from known hosts"
-ssh-keygen -R 192.168.88.10 && ssh-keygen -R 192.168.88.11 && ssh-keygen -R 192.168.89.12
+ssh-keygen -R 192.168.88.10 && ssh-keygen -R 192.168.88.11 && ssh-keygen -R 192.168.88.12
 vagrant up
 echo "Temporarily disabling Ansible host key checking for this terminal via env variable"
 export ANSIBLE_HOST_KEY_CHECKING=False
